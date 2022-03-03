909233f53c7d6b4633012f4c5060e42984ba9189
TUCchkul <42653001+TUCchkul@users.noreply.github.com>
2022-03-03 14:47:52 +0100

    mgt

--- src/stage_01_get_data.py
+++ src/stage_01_get_data.py
@@ -3,9 +3,10 @@ import os
 import shutil
 from tqdm import tqdm
 import logging
-from src.utils.common import read_yaml, create_directories
+from src.utils.common import read_yaml, create_directories, unzip_file
 import random
 import urllib.request as req 
+from src.utils.data_mgmt import validate_image
 
 
 
@@ -35,7 +36,11 @@ def main(config_path):
         logging.info(f"filename: {data_file} already present")
     #print(config)
     #params = read_yaml(params_path)
-    pass
+    unzip_data_dir=config["data"]["unzip_data_dir"]
+    create_directories([unzip_data_dir])
+    #unzip_data_dir_path=os.path.join(local_dir)
+    unzip_file(source=data_file_path, dest=unzip_data_dir)
+    validate_image(config)
 
 
 if __name__ == '__main__':
